ac3c89144621afc33c4cc29f6c2b57553d88ca2b
Carlos0599 <joseaguilar0522@gmail.com>
2023-03-31 11:17:38 -0600

    update: Tipo de creditos tienen secuencia

--- financial_credit/models/tipo_credito.py
+++ financial_credit/models/tipo_credito.py
@@ -8,3 +8,4 @@ class TiposCreditos(models.Model):
     interes = fields.Float("Interés Mensual")
     max_financiar = fields.Float("Porcentaje máximo a financiar", help="Si no requiere de un monto inicial dejarlo en 100%")
     currency_id = fields.Many2one('res.currency', readonly=True,default=lambda self:self.env.user.company_id.currency_id)
+    sequence = fields.Integer("Secuencia")
